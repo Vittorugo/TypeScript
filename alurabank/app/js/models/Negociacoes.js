7d759fa7e592b6c568cbc3a906914de0cd81cc12
victtor hugo <hgloureirocosta@gmail.com>
2020-10-27 19:03:13 -0300

    modularizando o código

--- alurabank/app/js/models/Negociacoes.js
+++ alurabank/app/js/models/Negociacoes.js
@@ -1,12 +1,23 @@
-class Negociacoes {
-    constructor() {
-        this._negociacoes = [];
-    }
-    adicionar(negociacao) {
-        this._negociacoes.unshift(negociacao);
-    }
-    paraArray() {
-        const newArray = this._negociacoes.slice();
-        return newArray;
-    }
-}
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var Negociacoes;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [],
+        execute: function () {
+            Negociacoes = class Negociacoes {
+                constructor() {
+                    this._negociacoes = [];
+                }
+                adicionar(negociacao) {
+                    this._negociacoes.unshift(negociacao);
+                }
+                paraArray() {
+                    const newArray = this._negociacoes.slice();
+                    return newArray;
+                }
+            };
+            exports_1("Negociacoes", Negociacoes);
+        }
+    };
+});
