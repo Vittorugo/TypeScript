c102dea88f5c34660293b73a9ad4aae412a70f52
victtor hugo <hgloureirocosta@gmail.com>
2020-10-27 00:34:59 -0300

    Abstraindo mais as classes

---
+++ alurabank/app/ts/views/View.ts
@@ -0,0 +1,14 @@
+abstract class View<T> {
+
+   private _elemento: Element;
+
+   constructor(seletor: string){
+      this._elemento = document.querySelector(seletor);
+   }
+
+   update(modelo: T): void{
+      this._elemento.innerHTML = this.template(modelo);
+   }
+
+   abstract template(modelo: T): string;
+}
